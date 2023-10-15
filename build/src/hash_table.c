9e8b00babb991aa8a4b00f033ed4467cd704031b
alvarorichard <alvaro10048@protonmail.com>
2023-10-15 14:49:46 -0300

    hashtab implementation

--- build/src/hash_table.c
+++ build/src/hash_table.c
@@ -47,3 +47,12 @@ static int ht_hash(const char* s, const int a, const int m){
     hash = hash % m;
   }
 }
+
+
+static int ht_get_hash (
+  const char* s, const int num_buckets, const int attempt
+){
+  const int hash_a = ht_hash(s, HT_PRIME_1, num_buckets);
+  const int hash_b = ht_hash(s, HT_PRIME_2,num_buckets);
+  return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
+}
